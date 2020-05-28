76ca9ef2c688e9ebb2aab40ff797d37f37f166fb
Christina Sohn <chsohn15@gmail.com>
2020-05-28 23:54:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,6 @@ def using_push(array, string)
   array.push(string)
 end
 
-using_push([1, 2], "3")
-
 def using_unshift(array, string)
   array.unshift(string)
 end
@@ -12,6 +10,6 @@ def using_pop(array)
   array.pop()
 end
 
-def pop_with_args(Array, item=2)
+def pop_with_args(array, item=2)
     array.pop(item)
 end
